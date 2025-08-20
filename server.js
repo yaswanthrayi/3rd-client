c6f48e3a4351f869405e5bc0a22c8eb24ca86e2b
Aditya <aditya@example.com>
2025-08-20 18:25:16 +0530

    initial

--- server.js
+++ server.js
@@ -25,4 +25,5 @@ app.post('/api/create-order', async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
